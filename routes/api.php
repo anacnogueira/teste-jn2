3f09768a608937e4ce41080bf4925e00144501cd
Ana Claudia <anacnogueira@gmail.com>
2023-08-14 12:32:10 -0300

    Adiciona rotas e cria controller de cliente

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ClientController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::post('/cliente-cadastro', [ClientController::class, 'store']);
+Route::put('/cliente/{id}', [ClientController::class, 'update']);
+Route::delete('/cliente/{id}', [ClientController::class, 'destroy']);
+Route::get('/cliente/{id}', [ClientController::class, 'show']);
+Route::get('/consulta/final-placa/{numero}', [ClientController::class, 'index']);
